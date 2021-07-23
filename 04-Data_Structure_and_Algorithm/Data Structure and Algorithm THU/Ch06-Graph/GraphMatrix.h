39b0b03eb46661ae5f1720fd77da9a1544648393
Chen Zhou <chenzhoux93@gmail.com>
2021-07-23 08:18:47 -0400

    Ch06-Graph DFS

--- 04-Data_Structure_and_Algorithm/Data Structure and Algorithm THU/Ch06-Graph/GraphMatrix.h
+++ 04-Data_Structure_and_Algorithm/Data Structure and Algorithm THU/Ch06-Graph/GraphMatrix.h
@@ -180,3 +180,38 @@ void Graph<Tv, Te>::BFS(int v, int& clock){//广度优先搜索BFS算法 单�
     }
 
 }
+
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::dfs(int s){
+    reset();
+    int clock = 0;
+    int v = s;
+    do{
+        if(UNDISCOVERED == status(v)){
+            DFS(v, clock);
+        }
+    }while(s != (v = (v++ % n)));
+}
+
+template<typename Tv, typename Te>
+void Graph<Tv, Te>::DFS(int v, int& clock){
+    dTime(v) = ++clock;
+    status(v) = DISCOVERED;
+    for(int u=firstNbr(v); -1<u; u=nextNbr(v,u)){
+        switch(status(u)){
+            case UNDISCOVERED:
+                type(v,u) = TREE;
+                parent(u) = v;
+                DFS(u, clock);
+                break;
+            case DISCOVERED:
+                type(v,u) = BACKWARD;
+                break;
+            default:
+                type(v,u) = (dTime(v) < dTime(u) ? FORWARD : CROSS);
+                break;
+        }
+        status(v) = VISITED;
+        fTime(v) = ++clock;
+    }
+}
