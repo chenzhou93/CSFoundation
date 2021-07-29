fc6c9b3616a5203700775a81deb16ed3f2b2bf93
Chen Zhou <chenzhoux93@gmail.com>
2021-07-29 07:50:28 -0400

    Ch07 BST insert

--- 04-Data_Structure_and_Algorithm/Data Structure and Algorithm THU/Ch07-BinarySearchTree/BST.h
+++ 04-Data_Structure_and_Algorithm/Data Structure and Algorithm THU/Ch07-BinarySearchTree/BST.h
@@ -49,3 +49,15 @@ template <typename T>
 BinNodePosi(T)& BST<T>::search(const T& e){
     return searchIn(_root, e, _hot=NULL);
 }
+
+template <typename T>
+BinNodePosi(T) BST<T>::insert(const T& e){
+    BinNodePosi(T)& x = search(e);
+    if(x){
+        return x;
+    }
+    x = new BinNode<T>(e, _hot);
+    _size++;
+    updateHeightAbove(x);
+    return x;
+}
