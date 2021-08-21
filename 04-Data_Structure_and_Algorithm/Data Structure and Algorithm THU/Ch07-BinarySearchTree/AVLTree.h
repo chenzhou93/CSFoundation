34f224cad8e0e681847abe48813fc05f181a39d2
Chen Zhou <chenzhoux93@gmail.com>
2021-08-21 12:09:06 -0400

    AVL Tree Insert

--- 04-Data_Structure_and_Algorithm/Data Structure and Algorithm THU/Ch07-BinarySearchTree/AVLTree.h
+++ 04-Data_Structure_and_Algorithm/Data Structure and Algorithm THU/Ch07-BinarySearchTree/AVLTree.h
@@ -18,6 +18,25 @@
 template <typename T>
 class AVL: public BST<T>{
     public:
-    BinNodePosi(T) insert(const & e);
+    BinNodePosi(T) insert(const T& e);
     bool remove(const T& e);
 };
+
+template <typename T>
+BinNodePosi(T) AVL<T>::insert(const T& e){
+    BinNodePosi(T)& x = search(e);
+    if(x){
+        return x;
+    }
+    BinNodePosi(T) xx = x = new BinNode<T>(e, _hot); 
+    _size++;
+    for(BinNodePosi(T) g=_hot; g; g=g->parent){
+        if(!AvlBalanced(*g)){
+            FromParentTo(*g) = rotateAt(tallerChild(tallerChild(g)));
+            break;
+        }else{
+            updateHeight(g);
+        }
+    }
+    return xx;
+}
