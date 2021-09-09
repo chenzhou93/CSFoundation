b4990302568c3531d1aa289bc35215cc0a00eed3
Chen Zhou <chenzhoux93@gmail.com>
2021-09-09 08:52:15 -0400

    insert node in BTREE

--- 04-Data_Structure_and_Algorithm/Data Structure and Algorithm THU/Ch08-AdvancedSearchTree/BTree.h
+++ 04-Data_Structure_and_Algorithm/Data Structure and Algorithm THU/Ch08-AdvancedSearchTree/BTree.h
@@ -35,3 +35,18 @@ class BTree{
     bool insert(const T& e);
     bool remove(const T& e);
 };
+
+template <typename T>
+BTNodePosi(T) BTree<T>::search(const T& e){
+    BTNodePosi(T) v = _root;
+    _hot = NULL;
+    while(v){
+        Rank r = v->key.search(e);
+        if((0<=r)&&(e==v->key[r])){
+            return v;
+        }
+        _hot = v;
+        v = v->child[r+1];
+    }
+    return NULL;
+}
